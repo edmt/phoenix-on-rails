9e46b86c4b7509473b68dbd1c574df88f74b154e
Urielable <uriel85@gmail.com>
2015-05-01 01:32:37 -0500

    Fixed typo error in model and add custom primary key

--- ruby-rails/db/migrate/20150430045430_create_books.rb
+++ ruby-rails/db/migrate/20150430045430_create_books.rb
@@ -1,7 +1,8 @@
 class CreateBooks < ActiveRecord::Migration
   def change
-    create_table :books do |t|
-      t.string :tittle
+    create_table :books, :id => false do |t|
+      t.column :book_id, 'serial NOT NULL PRIMARY KEY'
+      t.string :title
       t.string :description
       t.string :author
 
