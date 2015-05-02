83fb06db87c5a55ff569cd005514226f2675a6b1
Urielable <uriel85@gmail.com>
2015-05-02 15:56:14 -0500

    Remove phoenix app for conflict

--- phoenix/test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Phoenix.PageControllerTest do
-  use Phoenix.ConnCase
-
-  test "GET /" do
-    conn = get conn(), "/"
-    assert conn.resp_body =~ "Welcome to Phoenix!"
-  end
-end
--- phoenix/web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule Phoenix.PageController do
-  use Phoenix.Web, :controller
-
-  plug :action
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
