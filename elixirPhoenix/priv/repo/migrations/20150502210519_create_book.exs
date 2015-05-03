0bbe9f6653ad6c9710db08563d8dee9d66065b31
Urielable <uriel85@gmail.com>
2015-05-02 21:42:13 -0500

    Adds custom primary key in phoenix app

--- elixirPhoenix/priv/repo/migrations/20150502210519_create_book.exs
+++ elixirPhoenix/priv/repo/migrations/20150502210519_create_book.exs
@@ -3,7 +3,7 @@ defmodule ElixirPhoenix.Repo.Migrations.CreateBook do
 
   def change do
     create table(:books, primary_key: false) do
-      add :book_id    , :integer
+      add :book_id    , :integer, primary_key: true
       add :description, :string
       add :title      , :string
       add :author     , :string
